6125e7236d685b527a08c5640311c12ccf6653ad
Michael Patel <mikepatel@users.noreply.github.com>
2019-05-09 12:39:58 -0400

    Adding file to hold constants and globals

---
+++ constants.py
@@ -0,0 +1,21 @@
+"""
+Michael Patel
+May 2019
+
+version: Python 3.6.5
+
+File Description:
+
+Notes:
+
+"""
+
+
+################################################################################
+# Imports
+import os
+
+
+################################################################################
+OUTPUT_DIR = os.path.join(os.getcwd(), "Results")
+PLAYERS_CSV = os.path.join(os.getcwd(), "players list.csv")
