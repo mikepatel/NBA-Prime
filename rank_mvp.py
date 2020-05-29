317551c526c6ba785b8ddf001b5c9cc0ef727a4d
Michael Patel <michael.patel94@gmail.com>
2020-05-29 01:13:30 -0400

    Adding init csv for mvp

---
+++ rank_mvp.py
@@ -0,0 +1,11 @@
+"""
+Michael Patel
+May 2020
+
+Project description:
+    Use basketball reference data to rank every MVP winner's campaign since 2000
+
+File description:
+    Rank every MVP winner's campaign since 2000
+"""
+################################################################################
