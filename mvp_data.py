313a939cdead8a6c1350f4e2ce0f95a84aaa777f
Michael Patel <michael.patel94@gmail.com>
2020-05-29 01:40:14 -0400

    Adding get name

--- mvp_data.py
+++ mvp_data.py
@@ -11,13 +11,48 @@ File description:
 ################################################################################
 # Imports
 import os
+import re
 import pandas as pd
+import urllib.request
+from bs4 import BeautifulSoup
 
 from constants import *
 
 
 ################################################################################
+class Player:
+    def __init__(self, url):
+        self.url = url
+        with urllib.request.urlopen(self.url) as response:
+            page = response.read()
+
+        page = re.sub('<!--|-->', "", str(page))
+
+        # html soup
+        self.soup = BeautifulSoup(page, "html.parser")
+
+        # get player name
+        self.name = self.get_name()
+
+    # get name
+    def get_name(self):
+        name = self.soup.title.text
+        name, _ = name.split("Stats")
+        name = name.replace("\\", "")
+        name = name.strip()
+        return name
+
+
+################################################################################
 # Main
 if __name__ == "__main__":
+    # Read in CSV
     df = pd.read_csv(MVP_CSV)
-    print(df)
+
+    url_list = df["URL"]
+    url = url_list[0]
+
+    p = Player(url)
+    print(p.name)
+
+    # Write to CSV
