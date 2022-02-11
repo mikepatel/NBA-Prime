0bb3e5084f2fe87659bee6dfda161d5d52335f5d
Michael <michael.patel94@gmail.com>
2022-02-11 11:40:35 -0500

    Stage 2: On load, display image using JS

---
+++ Primes/web_app/app.js
@@ -0,0 +1,7 @@
+// On load
+window.addEventListener("DOMContentLoaded", function(){
+    // find img element and set attributes
+    const image = document.querySelector("img");
+    image.setAttribute("src", "images/LeBron James_plots.png");
+    image.setAttribute("alt", "LeBron James");
+});
